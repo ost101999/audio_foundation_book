01a792444f5edf6ad5cb5256686ded4b5175b24a
ost101999 <ost101999@gmail.com>
2026-05-08 21:52:42 +0300

    Add files via upload

---
+++ كتاب التأسيس - مسموع/firebase-config.js
@@ -0,0 +1,12 @@
+window.FIREBASE_CONFIG = {
+    apiKey: "AIzaSyBzhGlkfBG3iob4NdkA5zPd3dRqNq4ju7g",
+  authDomain: "audio-foundation-book.firebaseapp.com",
+  projectId: "audio-foundation-book",
+  storageBucket: "audio-foundation-book.firebasestorage.app",
+  messagingSenderId: "132959327913",
+  appId: "1:132959327913:web:c92564d98fc635a73abbb8",
+  measurementId: "G-07PXFNVPYV"
+};
+
+// حاليا خلّيه false لأننا شغالين بـ workflow رفع يدوي للصوت.
+window.USE_FIREBASE_STORAGE = false;
